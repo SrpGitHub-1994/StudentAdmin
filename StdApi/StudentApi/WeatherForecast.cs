c12f02f8233786d5462bfe12144b984d823ac655
shaharukhgit <shaharukhpathan786@gmail.com>
2022-03-19 15:37:49 +0530

    changed

--- StdApi/StudentApi/WeatherForecast.cs
+++ StdApi/StudentApi/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace StdApi
+using System;
+
+namespace StudentApi
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace StdApi
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
